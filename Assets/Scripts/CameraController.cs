a9587ad15eca0d47e231572c4f7532dc5820093e
Morgan <magxm41@gmail.com>
2019-03-20 14:01:32 +0100

    Basic plane rotations

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace POTM
+{
+    public class CameraController : MonoBehaviour
+    {
+        [Tooltip("The player the camera has to follow")]
+        public PlaneController player;
+
+        [Tooltip("Temporary varibale. Sets the base distance of the camera with the player")]
+        public float baseDistanceFromPlayer;
+
+        [Tooltip("The distance will be calculated according to the planes speed")]
+        public float maxDist, minDist;
+        [Tooltip("Downwards angle of the camera")]
+        public float cameraAngle;
+        [Tooltip("hight difference between player and camera")]
+        public float cameraHeight;
+
+        [Tooltip("Max and min FOV. Fov will be calculated by your speed / acceleration / environment")]
+        public float maxFOV, minFOV;
+
+        private Camera cam;
+        private bool isTurningRight = false, isTurningLeft = false;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            cam = GetComponent<Camera>();
+            transform.position = player.transform.position + (-player.transform.forward * baseDistanceFromPlayer);
+            transform.position += player.transform.up * cameraHeight;
+            transform.Rotate(new Vector3(cameraAngle, 0, 0));
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            float speedDiff = player.maxSpeed - player.minSpeed;
+            float distDiff = maxDist - minDist;
+            float diffFov = maxFOV - minFOV;
+            float additionalDist = (player.currentSpeed * distDiff) / speedDiff;
+            float additionalFOV = (player.currentSpeed * diffFov) / speedDiff;
+            
+            //Change camera distance + reset height
+            transform.position = (player.transform.position + -player.transform.forward * (minDist + additionalDist) + player.transform.up * cameraHeight);
+
+            //Set FOV according to speed
+            cam.fieldOfView = minFOV + additionalFOV;
+        }
+
+        public void RightOffset()
+        {
+
+            /// /!\
+            /// Add boolean in PlaneController to use as an event
+            /// This is done on top of the turning in plane controller
+
+            if (!isTurningRight)
+            {
+                transform.Rotate(new Vector3(-cameraAngle, 0, 0));
+                transform.Rotate(new Vector3(0, 5, 0));
+                transform.Rotate(new Vector3(cameraAngle, 0, 0));
+                isTurningRight = true;
+            }
+        }
+
+        public void LeftOffset()
+        {
+            if (!isTurningLeft)
+            {
+                transform.Rotate(new Vector3(-cameraAngle, 0, 0));
+                transform.Rotate(new Vector3(0, -5, 0));
+                transform.Rotate(new Vector3(cameraAngle, 0, 0));
+                isTurningLeft = true;
+            }
+        }
+
+        public void ResetOffset()
+        {
+            if (isTurningLeft)
+            {
+                transform.Rotate(new Vector3(-cameraAngle, 0, 0));
+                transform.Rotate(new Vector3(0, 5, 0));
+                transform.Rotate(new Vector3(cameraAngle, 0, 0));
+                isTurningLeft = false;
+            }
+            if(isTurningRight)
+            {
+                transform.Rotate(new Vector3(-cameraAngle, 0, 0));
+                transform.Rotate(new Vector3(0, -5, 0));
+                transform.Rotate(new Vector3(cameraAngle, 0, 0));
+                isTurningRight = false;
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/PlaneController.cs
@@ -0,0 +1,168 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace POTM
+{
+    public class PlaneController : MonoBehaviour
+    {
+        [Tooltip("The min and max speed of the plane")]
+        public float minSpeed, maxSpeed;
+        [Tooltip("how fast the plane turns sideways")]
+        public float turnSpeed;
+        [Tooltip("Speed at which the plane changes it pitchs")]
+        public float dipSpeed;
+        [Tooltip("Angle at which the plane stays at a constant speed")]
+        public float balanceAngle;
+        [Tooltip("How fast the plane accelerates")]
+        public float accelRatio;
+        [Tooltip("How long it takes for the turning animation to be complete")]
+        public float timeToBarrel;
+
+        [Tooltip("UI for info")]
+        public Text display;
+
+        private Rigidbody planeRB;
+        [HideInInspector]public float currentSpeed;
+        private CameraController cam;
+        private MeshRenderer planeMesh;
+
+        private float pitchAngle;
+        private bool right = false,
+            left = false;
+
+        private float lerpLength;
+        private float rollStartAngle;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            planeRB = GetComponent<Rigidbody>();
+            cam = GetComponentInChildren<CameraController>();
+            planeMesh = GetComponentInChildren<MeshRenderer>();
+            currentSpeed = (maxSpeed + minSpeed) / 2;
+            planeRB.velocity = transform.forward * maxSpeed;
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            //Get controls
+            float yaw = Input.GetAxis("Horizontal");
+            float pitch = Input.GetAxis("Vertical");
+
+
+            //Move bcack to horizontal, turn, and go back
+            if (yaw != 0)
+            {
+                float currentRotation = transform.rotation.eulerAngles.x;
+                transform.Rotate(new Vector3(-currentRotation, 0, 0));
+                transform.Rotate(new Vector3(0, yaw * turnSpeed, 0));
+                transform.Rotate(new Vector3(currentRotation, 0, 0));
+
+                //turn the model To the left or right (barrel roll)
+
+                /***********
+                 * ADAPT TO JOYSTICK (intermediate turning)
+                 * *********/
+
+                if (yaw > 0)
+                {
+                    if (left)
+                    {
+                        left = false;
+                    }
+                    resetRoll();
+                    planeMesh.transform.Rotate(new Vector3(0, 0, yaw * 33));
+                    //cam.RightOffset();
+                    right = true;
+                }
+
+                if (yaw < 0)
+                {
+                    if (right)
+                    {
+                        right = false;
+                    }
+                    resetRoll();
+                    planeMesh.transform.Rotate(new Vector3(0, 0, yaw * 33));
+                    //cam.LeftOffset();
+                    left = true;
+                }
+            }
+            else
+
+            {
+                cam.ResetOffset();
+
+                if (right)
+                {
+                    resetRoll();
+                    right = false;
+                }
+                if (left)
+                {
+                    resetRoll();
+                    left = false;
+                }
+            }
+
+            //Down
+            if (((transform.rotation.eulerAngles.x < 85) || (transform.rotation.eulerAngles.x > 270)) && pitch > 0)
+            {
+                transform.Rotate(new Vector3(pitch * dipSpeed, 0, 0));
+            }
+
+            //Up
+            if ((((transform.rotation.eulerAngles.x > 277) || (transform.rotation.eulerAngles.x < 87)) && pitch < 0))
+            {
+                transform.Rotate(new Vector3(pitch * dipSpeed, 0, 0));
+            }
+
+            rollStartAngle = planeMesh.transform.rotation.eulerAngles.z;
+
+            //Update the speed and pitchAngle varibale
+            updateSpeed();
+
+            //Update the display text
+            if (display != null)
+                updateDisplay();
+        }
+
+        //Changes the speed depending on it's pitch angle
+        //Has to be called in FixedUpdate ONLY ! affected by FPS;
+        public void updateSpeed()
+        {
+            //Get plane's angle to set it's acceleration
+            pitchAngle = transform.rotation.eulerAngles.x;
+            if (pitchAngle > 200) pitchAngle -= 360;
+            //Normaalize
+            pitchAngle /= 90;
+
+            //Calculate its acceleration
+            float speedVariation = pitchAngle * accelRatio * Time.deltaTime;
+            if (pitchAngle < 0) speedVariation *= 0.8f;
+            currentSpeed += speedVariation;
+
+            //Speed limits
+            if (currentSpeed < minSpeed) currentSpeed = minSpeed;
+            if (currentSpeed > maxSpeed) currentSpeed = maxSpeed;
+
+            planeRB.velocity = transform.forward * currentSpeed;
+
+            //Change camera to look where you are turning
+            
+        }
+
+        public void updateDisplay()
+        {
+            display.text = "Speed : " + currentSpeed + "\nAngle : " + pitchAngle + "\n Roll angle : " + rollStartAngle ;
+        }
+
+        public void resetRoll()
+        {
+            planeMesh.transform.rotation = Quaternion.Euler(planeMesh.transform.rotation.eulerAngles.x, planeMesh.transform.rotation.eulerAngles.y, 0);
+        }
+    }
+}
