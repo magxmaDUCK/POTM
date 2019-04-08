bc6df3b3b6ee777f3e610bf180b7ceed65775514
Morgan <magxm41@gmail.com>
2019-04-08 18:44:37 +0200

    Mesh generator working

--- Assets/Scripts/Splines/MeshGenerator/MeshGenerator.cs
+++ Assets/Scripts/Splines/MeshGenerator/MeshGenerator.cs
@@ -6,65 +6,72 @@ public class MeshGenerator : MonoBehaviour
 {
     public BezierSpline path;
 
-    [Range(1,100)]
+    public Material mat;
+
+    public float meshWidth;
+
+    
     public int subDivisions;
 
-    public Mesh mesh;
+    //public Mesh mesh;
 
     private void Awake()
     {
-        //multiply by 3 because 3 points at each subdiv;
-        Vector3[] vertices = new Vector3[subDivisions * 3];
-        int[] triangles = new int[(2 + 6 * subDivisions) * 3];
-        mesh = new Mesh();
+        GameObject go = new GameObject("Wire Mesh");
 
-        for(int i = 0; i <= subDivisions; i++)
+        MeshFilter meshFilter = go.AddComponent<MeshFilter>();
+        MeshRenderer meshRenderer = go.AddComponent<MeshRenderer>();
+
+        go.transform.SetParent(transform, false);
+
+        Mesh mesh = new Mesh();
+
+        mesh.vertices = WireVertices();
+        mesh.triangles = WireTriangles();
+        mesh.RecalculateNormals();
+
+        meshRenderer.material = mat;
+
+        meshFilter.sharedMesh = mesh;
+    }
+    
+    Vector3[] WireVertices()
+    {
+        Vector3[] vertices = new Vector3[subDivisions * 4];
+
+        for(int z = 0, i = 0; z < subDivisions; z++)
         {
-            float t = (float)i / (float)subDivisions;
-            Vector3 splinePointPos = path.GetPoint(t);
-            Vector3 splinePointDir = path.GetDirection(t);
+            float t = Mathf.InverseLerp(0, subDivisions - 1, z);
+            Vector3 center = path.GetPoint(t);
+            Vector3 direction = path.GetDirection(t);
 
-            //up, down left, down right
-            vertices[i] = new Vector3(0, 1, 0) + splinePointPos;
-            vertices[i + 1] = new Vector3(-.5f, -.5f, 0) + splinePointPos;
-            vertices[i + 2] = new Vector3(.5f, -.5f, 0) + splinePointPos;
-
-            if(i == 0)
+            for(int x = 0; x < 4; x++, i++)
             {
-                triangles[i] = i;
-                triangles[i+1] = i+1;
-                triangles[i+2] = i+2;
-            }
-            else if(i == subDivisions)
-            {
-                //Tube and end face (same as lwoer + upper)
-            }
-            else
-            {
-                //Tube (make with precedent points)
+                float angle = Mathf.InverseLerp(0, 3, x) * 360;
+                Quaternion rotation = Quaternion.LookRotation(direction) * Quaternion.Euler(Vector3.forward * angle);
+                vertices[i] = (rotation * Vector3.up * (meshWidth) + transform.InverseTransformPoint(center));
             }
         }
 
-        mesh.vertices = vertices;
-        mesh.triangles = triangles;
+        return vertices;
     }
 
-    //Used for debugging, to delete !
-    private void Update()
+    int[] WireTriangles()
     {
-        Vector3 pos, dir, tan;
-        float t;
+        int numTriangles = ((subDivisions - 1) * 4) * 2;
+        int[] triangles = new int[numTriangles * 3];
 
-        for(int i = 0; i <= 10; i++)
+        for(int t = 3, i = 0; t < triangles.Length-6; t += 6, i += 1)
         {
-            t = (float)i / (float)10;
-            pos = path.GetPoint(t);
-            dir = path.GetDirection(t);
-            tan = path.GetNormal(t);
+            triangles[t] = i + 4;
+            triangles[t + 1] = i;
+            triangles[t + 2] = i+1;
 
-            //Debug.DrawLine(pos, pos + dir);
-            //Debug.DrawLine(pos, pos + tan);
-
+            triangles[t + 3] = i + 4 + 1;
+            triangles[t + 4] = i + 4;
+            triangles[t + 5] = i + 1;
         }
+
+        return triangles;
     }
 }
