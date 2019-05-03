41cf01ad4a2da15448d61ab778ac3dfbe7ac8df2
Morgan <magxm41@gmail.com>
2019-05-03 18:27:47 +0200

    FPcamera controller

---
+++ Assets/Scripts/Player/FirstPersonCameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonCameraController : MonoBehaviour
+{
+    public float HorizontalSens;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        float turn = Input.GetAxis("Horizontal");
+
+        transform.Rotate(new Vector3(60, 0, 0));
+        transform.Rotate(new Vector3(0, turn * HorizontalSens * Time.deltaTime, 0));
+        transform.Rotate(new Vector3(-60, 0, 0));
+    }
+}
