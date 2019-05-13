14420713478fe512f41b7a9fad6f72d0aef0244f
Morgan <magxm41@gmail.com>
2019-05-13 14:04:41 +0200

    Random light mat picker script

---
+++ Assets/Scripts/Lights/LightColorPicker.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace POTM
+{
+    public class LightColorPicker : MonoBehaviour
+    {
+        public LightColors colors;
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+            Renderer rend = GetComponent<Renderer>();
+            rend.material = colors.col[Random.Range(0, colors.col.Count)];
+        }
+    }
+}
---
+++ Assets/Scripts/Lights/LightColors.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace POTM
+{
+    [CreateAssetMenu(fileName = "ColorPicker", menuName ="Lights/Colors", order = 1)]
+    public class LightColors : ScriptableObject
+    {
+        public List<Material> col;
+    }
+}
