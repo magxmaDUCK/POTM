67fd21a43e5d6b73baabcfa2553a89c3ae38e2e7
Jeremy Guarober <jeremyguarober@hotmail.fr>
2019-05-17 14:58:52 +0200

    Modif scene son + script audio

---
+++ Assets/Scripts/Audio/SetMusState.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetMusState: MonoBehaviour
+{
+    [SerializeField] AK.Wwise.State ambState;
+
+    private void OnTriggerEnter(Collider other)
+    {        
+        {
+            ambState.SetValue();
+        }       
+    }
+}
