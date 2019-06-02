da4382eba5801f9cb77d456306c96d59364a4a61
Simon <simongirau@gmail.com>
2019-06-02 02:21:12 +0200

    islandlevel v1 ok + moon

---
+++ Assets/Scripts/Skybox/SkyBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class SkyBehaviour : MonoBehaviour
+{
+    public GameObject kite;
+    public float distance = 50f;
+
+    void LateUpdate()
+    {
+        transform.LookAt(transform.position + Camera.main.transform.rotation * Vector3.forward,
+        Camera.main.transform.rotation * Vector3.up);
+
+        transform.position = (transform.position - kite.transform.position).normalized * distance + kite.transform.position;
+    }
+}
