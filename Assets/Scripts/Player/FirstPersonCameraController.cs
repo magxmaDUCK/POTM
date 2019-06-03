c82d7ce8e988b8910fc1e0d9402e16d56f09305f
MagxmaDUCK <magxm41@gmail.com>
2019-06-03 23:12:42 +0200

    fade ending in starrysky

--- Assets/Scripts/Player/FirstPersonCameraController.cs
+++ Assets/Scripts/Player/FirstPersonCameraController.cs
@@ -2,23 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FirstPersonCameraController : MonoBehaviour
+namespace POTM
 {
-    public float HorizontalSens;
-    public Camera cineCam;
-
-    // Update is called once per frame
-    void FixedUpdate()
+    public class FirstPersonCameraController : MonoBehaviour
     {
-        float turn = Input.GetAxis("Horizontal");
-        Vector3 rot = transform.rotation.eulerAngles;
-
-        //transform.Rotate(-rot);
-        //transform.Local.Rotate(new Vector3(0, turn * HorizontalSens * Time.deltaTime, 0));
-        //transform.Rotate(rot);
-        transform.RotateAroundLocal(Vector3.up, turn * HorizontalSens * Time.deltaTime);
-        
-    }
+        public float HorizontalSens;
+        public Camera cineCam;
+        public LevelChanger lc;
+        private float startTime = 0f;
 
     
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            float turn = Input.GetAxis("Horizontal");
+            Vector3 rot = transform.rotation.eulerAngles;
+
+            //transform.Rotate(-rot);
+            //transform.Local.Rotate(new Vector3(0, turn * HorizontalSens * Time.deltaTime, 0));
+            //transform.Rotate(rot);
+            transform.RotateAroundLocal(Vector3.up, turn * HorizontalSens * Time.deltaTime);
+            
+
+            if(Time.time - startTime > 30.0f)
+            {
+                lc.FadeToLevel("IslandLevel");
+            }
+        }
+        
+        void OnEnable()
+        {
+            startTime = Time.time;
+        }
+    }
 }
